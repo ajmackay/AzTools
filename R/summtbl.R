f25b466370ff3e5e9979bfc5abdf04b97f0806ec
Aaron Mackay <amackay@dementia.com.au>
2023-02-15 10:27:07 +1100

    added summtbl

---
+++ R/summtbl.R
@@ -0,0 +1,29 @@
+#' Basic Summary Table
+#'
+#' @param dat A dataframe to summarise
+#' @param summ.by Variable to stratify summary table by
+#' @param dp Number of decimal places
+#' @param caption Table Caption
+#'
+#' @return A gtsummary table
+#' @export
+#'
+#' @examples
+#'
+#'
+summ.tbl <- function(dat, summ.by = NULL, dp = 1, caption = NULL){
+  tbl_summary(dat,
+              by = summ.by,
+              statistic = list(all_continuous() ~ "{mean} ({sd})"),
+              digits = list(
+                all_continuous() ~ dp,
+                all_categorical() ~ dp
+              ),
+              missing_text = "Missing",
+              sort = list(everything() ~ "frequency")) %>%
+    bold_labels() %>%
+    as_flex_table() %>%
+    fontsize(size = 10) %>%
+    theme_zebra() %>%
+    set_caption(caption)
+}
