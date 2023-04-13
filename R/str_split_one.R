0fda8ca953597c62cb22db39d5816d4c1f07ec29
Aaron Mackay <amackay@dementia.com.au>
2023-04-14 06:45:16 +1000

    str-one

---
+++ R/str_split_one.R
@@ -0,0 +1,20 @@
+#' Split a string
+#'
+#' @param string A character vector with, at most, one element.
+#' @inheritParams stringr::str_split
+#'
+#' @return A charactor vector
+#' @export
+#'
+#' @examples
+#' x <- "alpha,bravo,charlie,delta"
+#' str_split_one(x, pattern = ",")
+#' str_split_one(x, )
+str_split_one <- function(string, pattern, n = Inf){
+  stopifnot(is.character(string), length(string) <= 1)
+  if(length(string) == 1){
+    stringr::str_split(string = string, pattern = pattern, n = n)
+  } else {
+    character()
+  }
+}
