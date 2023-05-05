467c4bf3603d87dffdbae246c534824411503b4a
ajmackay <mackay_aaron@protonmail.com>
2023-05-05 15:57:03 +1000

    updates

---
+++ R/flextable.R
@@ -0,0 +1,38 @@
+#' Set Flextable Defaults
+#'
+#' @param font.family
+#' @param font.size
+#' @param layout
+#' @param padding
+#' @param theme_fun
+#' @param digits
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+set.default.ft <- function(font.family = 'Calibri',
+                           font.size = 10,
+                           layout = 'autofit',
+                           padding = 6,
+                           theme_fun = 'theme_vanilla',
+                           digits = 3, ...) {
+
+  flextable::set_flextable_defaults(
+    font.family = font.family,
+    font.size = font.size,
+    layout = layout,
+    padding = padding,
+    theme_fun = theme_fun,
+    digits = digits)
+
+  if(digits != 3){
+    cat(crayon::red("Use colformat_double() to adjust digits"))
+  }
+
+  cat(crayon::green("Flextable Default Set"))
+
+}
+
