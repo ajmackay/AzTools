42c931b0a1b3fd85e13de91439e2fe61e5dee45e
Aaron Mackay <amackay@dementia.com.au>
2023-07-03 09:07:24 +1000

    Added time extract function

---
+++ R/time-extract.R
@@ -0,0 +1,19 @@
+#' Extract Time Units
+#'
+#' @param data Dataframe with at least one datetime variable
+#' @param date_var Datetime variable to extract datetime unit from
+#' @param time_units The units of datetime to extract as a string e.g. c("year", "month", "day")
+#'
+#' @return A dataframe with specified datetime units
+#' @export
+#'
+#' @examples
+time.extract <- function(data, date_var, time_units) {
+  data %>%
+    timetk::tk_augment_timeseries_signature({{date_var}}) |>
+    select(
+      all_of(
+        c(colnames(data), time_units)
+      )
+    )
+}
--- R/utilities.R
+++ R/utilities.R
@@ -1,4 +1,25 @@
+#' Stop R session without error
+#'
+#'`stop_quietly()` does exactly the same as `stop()`, however it does not print anything to the console.
+#'
+#' @author Stuart K. Grange
+#'
+#' @return Invisible, the R session will be stopped.
+#'
+#' @export
+stop_quietly <- function() {
+
+  opt <- options(show.error.messages = FALSE)
+  on.exit(options(opt))
+  stop()
+
+}
+
+
 #' Load a Single Object
+#'
+#' `load.object()` Loads the specified name of the RDS object located in data/objects/.
+#' The .rds object must be assigned to an object in the workspace to be saved in the global environment.
 #'
 #' @param name File name of the object
 #'
@@ -10,6 +31,8 @@ load.object <- function(name){
 
 
 #' Save a single object
+#'
+#' `save.object()` Saves a specified object as a .rds file in data/objects/
 #'
 #' @param obj The object to Save
 #' @param name File name of the Object
@@ -23,6 +46,8 @@ save.object <- function(obj, name){
 }
 
 #' Load all objects
+#'
+#' `load.objects()` Loads a specified .RData file (default is "all-objects.RData") from data/objects/ into the global environment
 #'
 #' @param dataset String of the name of the dataset to load. Default is all-objects
 #' @return Loads all objects saved in all-objects.RData into the environment
@@ -70,54 +95,4 @@ load.packages <- function() {
 }
 
 
-create.project <- function (project.name, dir, github = TRUE, initial.commit = TRUE){
 
-  project.name <- str_trim(project.name)
-
-  project.dir <- str_glue("~/{dir}/{project.name}")
-
-  if(dir.exists(project.dir)) {
-    stop("Folder with this name already exists")
-  }
-
-  dir.create(project.dir)
-  setwd(project.dir)
-  .GlobalEnv$.working.d = project.dir
-  R.utils::copyDirectory(system.file("template", package = "dcutilities"),
-                         project.dir)
-
-
-  file.rename("PROJECT_NAME.Rproj", str_c(project.name, ".Rproj"))
-  clean.project.name = str_replace_all(project.name, " ", "-")
-  readLines("quarto-template.qmd") %>% str_replace("@REPORT-NAME-HTML",
-                                                   str_c(clean.project.name, ".html")) %>% str_replace("@REPORT-NAME-QMD",
-                                                                                                       str_c(clean.project.name, ".qmd")) %>% str_replace("@REPORT-NAME",
-                                                                                                                                                          project.name) %>% writeLines("quarto-template.qmd")
-  file.rename("quarto-template.qmd", str_c(clean.project.name,
-                                           ".qmd"))
-  document.name = str_replace_all(project.name, " ", "-")
-  readLines("word-template.qmd") %>% str_replace("@OUTPUT-NAME",
-                                                 str_c(document.name, ".docx")) %>% str_replace("@TEMPLATE-NAME",
-                                                                                                str_c(document.name, "_word.qmd")) %>% str_replace("@REPORT-NAME",
-                                                                                                                                                   project.name) %>% writeLines("word-template.qmd")
-  file.rename("word-template.qmd", str_c(project.name, "_word.qmd"))
-  saveRDS(list(), "./cache/outputs")
-  if (!is.null(default.queries)) {
-    library(rforce)
-    target.queries = map_chr(default.queries, function(query) {
-      switch(query, Cases = "cases", NPI = "npi", `Contributing Factors` = "contributing.factor",
-             Milestones = "milestones", `Milestone Breaches` = "kpi.breaches")
-    }) %>% str_c(collapse = "|")
-    tibble(queries = system.file("default.queries", package = "rforce") %>%
-             list.files(full.names = TRUE)) %>% filter(str_detect(queries,
-                                                                  target.queries)) %>% pull(queries) %>% walk(~file.copy(.x,
-                                                                                                                         "./cache/queries"))
-    .GlobalEnv$query.list = load.queries()
-  }
-  if (initial.commit) {
-    git2r::init()
-    git.add()
-    git.commit(message = "initial")
-  }
-  invisible(settings(reset = TRUE))
-}
