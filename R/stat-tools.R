bef8c68a55efb50c43f24095f13acbeebf6b5ec1
Aaron Mackay <amackay@dementia.com.au>
2024-05-31 15:32:22 +1000

    wilson score added

---
+++ R/stat-tools.R
@@ -0,0 +1,23 @@
+#' Wilson Score Confidence Intervals
+#' @description
+#' Calculates the low and high confidence intervals for proportions using the Wilson Score Interval Method.
+#'
+#'
+#' @param num Numerator of proportion
+#' @param denom Denominator of proportion
+#' @param ... Additional arguments passed to prop.test
+#'
+#' @return Tibble with conf.low and conf.high
+#' @export
+#'
+wilson_ci <- function(num, denom, ...) {
+  wilson_score <- map2(.x = num, .y =  denom, function(x, y) {
+    prop.test(x, y, correct = TRUE, ...) %>% tidy()
+  })
+  # browser()
+
+  tibble(
+    conf.low = map_dbl(wilson_score, ~.x$conf.low),
+    conf.high = map_dbl(wilson_score, ~.x$conf.high)
+  )
+}
